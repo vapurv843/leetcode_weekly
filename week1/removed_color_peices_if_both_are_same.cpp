045fbc5d052a411a98125de0d2e85f98db4ece0a
Apurv verma <vapurv843@gmail.com>
2021-11-29 23:24:46 +0530

    greedy

---
+++ week1/removed_color_peices_if_both_are_same.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int a=0,b =0;
+        for(int i = 1;i<colors.size()-1;i++)
+        {
+            if(colors[i-1]==colors[i] and colors[i+1]==colors[i])
+            {
+                if(colors[i]=='A')
+                {
+                    a++;
+                }
+                else
+                {
+                    b++;
+                }
+            }
+        }
+        return a>b;
+    }
+};
