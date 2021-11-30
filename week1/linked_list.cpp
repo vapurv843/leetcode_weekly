b6c665f7a16eb671419cf39760641b7770164875
Apurv verma <vapurv843@gmail.com>
2021-12-01 00:16:47 +0530

    linked list

---
+++ week1/linked_list.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node *next;
+    public:
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+node * head = NULL;
+node * tail = NULL;
+void push(int data){
+        if(head==NULL){
+            node *n = new node(data);
+            head =tail= n;
+
+        }
+        else
+        {
+            node *n = new node(data);
+            n->next = head;
+            head = n;
+        }
+    }
+
+void push_back(int data){
+    if(head==NULL){
+        node *n = new node(data);
+        head = n;
+    }
+    else{
+        node * n = new node(data);
+        tail->next = n;
+        tail = n;
+    }
+}
+int main()
+{
+    push(8);
+    push_back(19);
+    while(head){
+        cout<<head->data;
+        head = head->next;
+    }
+
+}
