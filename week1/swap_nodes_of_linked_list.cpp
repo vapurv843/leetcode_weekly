717e8d37aafb1edab8c0b120535abb84a8f14acc
Apurv verma <vapurv843@gmail.com>
2021-12-03 12:37:58 +0530

    linked list

---
+++ week1/swap_nodes_of_linked_list.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void makeArray(ListNode*head,vector<int> &ans)
+    {
+        while(head)
+        {
+            ans.push_back(head->val);
+            head = head->next;
+        }
+    }
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode * temp = head;
+        ListNode * temp2 = head;
+        
+        
+        vector<int> ans;
+        makeArray(temp,ans);
+        int n  = ans.size();
+        swap(ans[k-1],ans[n-k]);
+        int i = 0;
+        while(temp2)
+        {
+            temp2->val = ans[i];
+            i++;
+            temp2= temp2->next;
+        }
+        return head;
+        
+        
+    }
+};
