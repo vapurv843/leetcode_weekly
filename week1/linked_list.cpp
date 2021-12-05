d1c8387a0eb756677216545236b8c50fe22f9da0
Apurv verma <41693158+vapurv843@users.noreply.github.com>
2021-12-05 16:30:40 +0530

    Update linked_list.cpp

--- week1/linked_list.cpp
+++ week1/linked_list.cpp
@@ -1,66 +1,51 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std ;
 class node
 {
     public:
     int data;
     node *next;
-    public:
-    node(int d)
-    {
+    node(int d){
         data = d;
         next = NULL;
     }
+
 };
-node * head = NULL;
-node * tail = NULL;
-void push(int data){
-        if(head==NULL){
-            node *n = new node(data);
-            head =tail= n;
-
-        }
-        else
-        {
-            node *n = new node(data);
-            n->next = head;
-            head = n;
-        }
-    }
-
-void push_back(int data){
-    if(head==NULL){
+node *head = NULL;
+node *tail = NULL;
+void push(int data) {
+    if(head == NULL)
+    {
         node *n = new node(data);
         head = n;
+        tail = n;
     }
-    else{
+    else
+    {
+        node * n = new node(data);
+        n->next = head;
+        head = n;
+    }
+}
+void push_back(int data) {
+    if( head == NULL){
+        node * n = new node(data);
+        head =tail= n;
+    }
+    else
+    {
         node * n = new node(data);
         tail->next = n;
         tail = n;
     }
 }
-void inser_position(int data,int pos){
-    if(pos == 0){
-        node *n = new node(data);
-        n->next = head;
-        head = n;
-    }
-    else{
-        node *temp = head;
-        for(int jump = 1;jump<=pos-1;jump++){
-            temp = temp->next;
-        }
-        node *n =new node(data);
-        n->next = temp->next;
-        temp->next = n;
-    }
-}
 int main()
 {
     push(8);
+    push(9);
     push_back(19);
     while(head){
-        cout<<head->data;
+        cout<<head->data<<" ";
         head = head->next;
     }
 
